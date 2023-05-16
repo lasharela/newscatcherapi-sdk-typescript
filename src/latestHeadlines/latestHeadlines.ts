bc8d5729d6cd27e7cdef96294b83dd8770f61b34
Lasha Kvantaliani <lasharela@gmail.com>
2023-05-15 21:30:41 -0700

    add ts file

--- src/latestHeadlines/latestHeadlines.ts
+++ src/latestHeadlines/latestHeadlines.ts
@@ -1,25 +1,32 @@
-import { api } from '../base/base'
-import { formatProps } from '../utilities'
-import { LatestHeadlinesRequest, LatestHeadlinesRequestAPI, LatestHeadlinesResponse } from './latestHeadlines.type'
-import { AxiosResponse } from 'axios'
+import { api } from "../base/base";
+import { formatProps } from "../utilities";
+import {
+  LatestHeadlinesRequest,
+  LatestHeadlinesRequestAPI,
+  LatestHeadlinesResponse,
+} from "./latestHeadlines.type";
+import { AxiosResponse } from "axios";
 
 export const latestHeadlines = async (options?: LatestHeadlinesRequest) => {
-  const optionsForAPI: LatestHeadlinesRequestAPI = {}
+  const optionsForAPI: LatestHeadlinesRequestAPI = {};
 
   if (options?.lang) {
-    optionsForAPI.lang = formatProps(options.lang)
+    optionsForAPI.lang = formatProps(options.lang);
   }
 
   if (options?.countries) {
-    optionsForAPI.countries = formatProps(options.countries)
+    optionsForAPI.countries = formatProps(options.countries);
   }
 
   if (options?.not_countries) {
-    optionsForAPI.not_countries = formatProps(options.not_countries)
+    optionsForAPI.not_countries = formatProps(options.not_countries);
   }
 
-  return await api.post<unknown, AxiosResponse<LatestHeadlinesResponse>>('/latest_headlines', {
-    ...options,
-    ...optionsForAPI,
-  })
-}
+  return await api.post<unknown, AxiosResponse<LatestHeadlinesResponse>>(
+    "/latest_headlines",
+    {
+      ...options,
+      ...optionsForAPI,
+    }
+  );
+};
