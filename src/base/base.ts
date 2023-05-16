a596acf3f33ffd4174b657fe327330712f0ddd00
Lasha Kvantaliani <lasharela@gmail.com>
2023-05-15 23:03:31 -0700

    remove extra console.log

--- src/base/base.ts
+++ src/base/base.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { config } from "../config";
 
-console.log("KEEEY", config.NEWS_API_KEY);
-
 export const api = axios.create({
   baseURL: config.NEWS_API_URL,
   headers: { "x-api-key": config.NEWS_API_KEY },
