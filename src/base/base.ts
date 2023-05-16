a5fa1e9c76e2e06f515133e27865d43d51df6d7e
Lasha Kvantaliani <lasharela@gmail.com>
2023-05-15 19:35:10 -0700

    switch to microbundle

--- src/base/base.ts
+++ src/base/base.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
-import { config } from '../../config'
+import axios from "axios";
+import { config } from "../config";
 
-console.log('KEEEY', config.NEWS_API_KEY)
+console.log("KEEEY", config.NEWS_API_KEY);
 
 export const api = axios.create({
   baseURL: config.NEWS_API_URL,
-  headers: { 'x-api-key': config.NEWS_API_KEY },
-})
+  headers: { "x-api-key": config.NEWS_API_KEY },
+});
