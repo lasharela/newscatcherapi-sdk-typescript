255b7df00e22fe3bc3f42e2e68b35d642898c0cb
Lasha Kvantaliani <lasharela@gmail.com>
2023-05-16 19:55:49 -0700

    make media nullable

--- src/types/article.type.ts
+++ src/types/article.type.ts
@@ -1,21 +1,21 @@
 export interface Article {
-  title: string
-  author: string
-  published_date: string
-  published_date_precision: string
-  link: string
-  clean_url: string
-  excerpt: string | null
-  summary: string
-  rights: string
-  rank: number
-  topic: string
-  country: string
-  language: string
-  authors: string
-  media: string
-  is_opinion: boolean
-  twitter_account: string | null
-  _score: null
-  _id: string
+  title: string;
+  author: string;
+  published_date: string;
+  published_date_precision: string;
+  link: string;
+  clean_url: string;
+  excerpt: string | null;
+  summary: string;
+  rights: string;
+  rank: number;
+  topic: string;
+  country: string;
+  language: string;
+  authors: string;
+  media: string | null;
+  is_opinion: boolean;
+  twitter_account: string | null;
+  _score: null;
+  _id: string;
 }
